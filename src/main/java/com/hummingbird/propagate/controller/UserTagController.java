95f76285bcf05abaff24895039142c2421e701e9
xycpand <1578348806@qq.com>
2016-06-23 14:43:34 +0800

    修改

--- src/main/java/com/hummingbird/propagate/controller/UserTagController.java
+++ src/main/java/com/hummingbird/propagate/controller/UserTagController.java
@@ -29,7 +29,7 @@ public class UserTagController extends BaseController{
 	UserTagService userTagService;
 	//1. 查询用户的标签
 	@RequestMapping(value = "/queryUserTag", method = RequestMethod.POST)
-	@AccessRequered(methodName = "查询用户的标签接口", isJson = true, codebase = 245900, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.paas.vo.OpenidBodyVO", appLog = true)
+	@AccessRequered(methodName = "查询用户的标签接口", isJson = true, codebase = 245900, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.propagate.vo.OpenidBodyVO", appLog = true)
 	public @ResponseBody ResultModel queryUserTag(HttpServletRequest request,
 			HttpServletResponse response) {
 		ResultModel rm = super.getResultModel();
@@ -64,7 +64,7 @@ public class UserTagController extends BaseController{
 	
 	//2. 查询某文章的标签
 	@RequestMapping(value = "/queryArticleTag", method = RequestMethod.POST)
-	@AccessRequered(methodName = "查询某文章的标签", isJson = true, codebase = 245901, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.paas.vo.ArticleTagBodyVO", appLog = true)
+	@AccessRequered(methodName = "查询某文章的标签", isJson = true, codebase = 245901, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.propagate.vo.ArticleTagBodyVO", appLog = true)
 	public @ResponseBody ResultModel queryArticleTag(HttpServletRequest request,
 			HttpServletResponse response) {
 		ResultModel rm = super.getResultModel();
@@ -101,7 +101,7 @@ public class UserTagController extends BaseController{
 	
 	//3. 为用户的某篇文章添加标签
 	@RequestMapping(value = "/addArticleTag", method = RequestMethod.POST)
-	@AccessRequered(methodName = "为用户的某篇文章添加标签", isJson = true, codebase = 245902, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.paas.vo.AddArticleTagBodyVO", appLog = true)
+	@AccessRequered(methodName = "为用户的某篇文章添加标签", isJson = true, codebase = 245903, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.propagate.vo.AddArticleTagBodyVO", appLog = true)
 	public @ResponseBody ResultModel addArticleTag(HttpServletRequest request,
 			HttpServletResponse response) {
 		ResultModel rm = super.getResultModel();
@@ -137,11 +137,45 @@ public class UserTagController extends BaseController{
 	}
 	
 	//4. 为用户的某篇文章删除标签
-	
+	@RequestMapping(value = "/delArticleTag", method = RequestMethod.POST)
+	@AccessRequered(methodName = "为用户的某篇文章删除标签", isJson = true, codebase = 245903, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.propagate.vo.AddArticleTagBodyVO", appLog = true)
+	public @ResponseBody ResultModel delArticleTag(HttpServletRequest request,
+			HttpServletResponse response) {
+		ResultModel rm = super.getResultModel();
+		BaseTransVO<AddArticleTagBodyVO> transorder = (BaseTransVO<AddArticleTagBodyVO>) super.getParameterObject();
+		String messagebase = "为用户的某篇文章删除标签";
+
+		RequestEvent qe = null; // 业务请求事件,当实现一些关键的业务时,需要生成该请求
+
+		try {
+			// 业务数据必填等校验
+			// 业务数据逻辑校验
+			if (log.isDebugEnabled()) {
+				log.debug("检验通过，获取请求");
+			}
+			if(StringUtils.isBlank(transorder.getBody().getOpenid())){
+				log.error(String.format("openid不能为空",transorder.getBody().getOpenid()));
+				throw new TokenException("openid不能为空");
+			}
+			if(StringUtils.isBlank(transorder.getBody().getArticleId())){
+				log.error(String.format("articleid不能为空",transorder.getBody().getArticleId()));
+				throw new TokenException("articleid不能为空");
+			}
+			//添加标签
+			userTagService.delArticleTag(transorder.getBody());
+			rm.setErrmsg(messagebase+"成功");
+		} catch (Exception e1) {
+			log.error(String.format(messagebase + "失败"), e1);
+			rm.mergeException(e1);
+			if (qe != null)
+				qe.setSuccessed(false);
+		} 
+		return rm;
+	}
 	
 	//5. 查询热门标签
 	@RequestMapping(value = "/queryHotTag", method = RequestMethod.POST)
-	@AccessRequered(methodName = "查询热门标签", isJson = true, codebase = 245905, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.paas.vo.OpenidBodyVO", appLog = true)
+	@AccessRequered(methodName = "查询热门标签", isJson = true, codebase = 245905, className = "com.hummingbird.commonbiz.vo.BaseTransVO", genericClassName = "com.hummingbird.propagate.vo.OpenidBodyVO", appLog = true)
 	public @ResponseBody ResultModel queryHotTag(HttpServletRequest request,
 			HttpServletResponse response) {
 		ResultModel rm = super.getResultModel();
